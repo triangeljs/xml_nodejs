855c41f9cc2f6725a7ad26c31fce39ea5ae7e6ed
yangfan <yangfan_china@q.com>
2013-08-24 14:09:25 +0800

    修改了输出XML文件格式

--- routes/show.js
+++ routes/show.js
@@ -1,8 +1,15 @@
 var xmlUrl = null;
-var arrs = [];
 var jsdom = require("jsdom");
 var fs = require("fs");
 var jquery = fs.readFileSync("./public/javascripts/jquery-1.9.1.min.js", "utf-8");
+var JSON = {
+  'title' : 'xml数据',
+  'link' : 'http://yangfan.com:3000/',
+  'description' : 'XML数据摘要',
+  'author' : '作者',
+  'pubDate' : '时间',
+  item : []
+};
 
 exports.list = function (req, res) {
   //dataUrl:http://www.china.com.cn/node_7187073.htm
@@ -16,10 +23,19 @@ exports.list = function (req, res) {
     src: [jquery],
     done: function (errors, window) {
       var $ = window.$;
+      JSON.item = [];
       $('ol li').each(function(index, element){
-        arrs.push($(this).html());
-      })
-      res.send(arrs[0]);
+        console.log(index);
+        JSON.item.push({
+          title : $(this).find('h1').html(),
+          link : $(this).find('h5').html(),
+          description : $(this).find('h3').html(),
+          author : $(this).find('h6').html(),
+          pubDate : $(this).find('h2').html()
+        });
+      });
+      res.setHeader("Content-Type", "text/xml");
+      res.render('show', JSON);
     }
   });
 };
